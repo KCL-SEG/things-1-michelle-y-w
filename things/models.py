4a60d62650e02593ca98e39ef9195576f7b5bc28
Michelle Yue Wang <yue.7.wang@kcl.ac.uk>
2022-10-09 23:10:07 +0100

    test

--- things/models.py
+++ things/models.py
@@ -2,10 +2,10 @@ from django.db import models
 from django.db.models import IntegerField, Model
 from django.core.validators import MaxValueValidator, MinValueValidator
 
-class Thing(models.Model):
+class Thing(Model):
     name = models.CharField(
         max_length=30,
-        #unique=True,
+        unique=True,
         blank=False
     )
     description = models.CharField(
