fd7e5344c4a487ce9a36e918ebf8f3883e121bfe
Michelle Yue Wang <yue.7.wang@kcl.ac.uk>
2022-10-09 22:10:54 +0100

    test

--- things/models.py
+++ things/models.py
@@ -1,8 +1,6 @@
 from django.db import models
-from django.db.models.Model import models
 
 class Thing(models.Model):
     name = models.CharField()
     description = models.TextField()
     quantity = models.integerField()
-    
